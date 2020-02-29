1892fe53443a44213df7f5996311d3a206b26b61
Vithor <vithort@gmail.com>
2020-02-29 20:07:12 -0300

    25. Classes de mensagens com alert
    Classes de mensagens com alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+  styleUrls: ['./alert.page.scss']
 })
 export class AlertPage implements OnInit {
+  constructor(
+    public alertController: AlertController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
-
 }
