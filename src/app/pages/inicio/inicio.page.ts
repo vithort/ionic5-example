e91db7efc0192d4de4a415097ede25708a638431
Vithor <vithort@gmail.com>
2020-02-29 19:05:49 -0300

    17. Criando um array para listas
    Criando um array para listas

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  styleUrls: ['./inicio.page.scss']
 })
 export class InicioPage implements OnInit {
+  componentes: Componente[] = [
+    {
+      icone: 'imagem',
+      nome: 'Alerta',
+      link: '/alerta'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+}
 
+interface Componente {
+  icone: string;
+  nome: string;
+  link: string;
 }
