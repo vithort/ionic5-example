2d78ad260efa3b2f631d7f942185e2040f171d78
Vithor <vithort@gmail.com>
2020-02-29 22:55:49 -0300

    47. Propriedades do Toast
    Propriedades do Toast

--- src/app/pages/toast/toast.page.ts
+++ src/app/pages/toast/toast.page.ts
@@ -23,4 +23,29 @@ export class ToastPage implements OnInit {
     toast.present();
   }
 
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
 }
